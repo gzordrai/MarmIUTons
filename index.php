19d4ba75aacb82f8b3fcdb4b73bceafbfad8db21
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-01-10 00:42:21 +0100

    processing of actions by the CilentController

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require_once("./conf/Connexion.php");
+require_once("./controller/ClientController.php");
 
 if(session_status() == 1) {
     session_start();
@@ -11,4 +12,5 @@ $action = "home";
 if (isset($_GET["action"]) && in_array($_GET["action"], get_class_methods("ClientController")))
     $action = $_GET["action"];
 
+ClientController::$action();
 ?>
