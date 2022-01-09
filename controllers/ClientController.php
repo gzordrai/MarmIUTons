40da6486ab3e8058342e6d52207129534d173486
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-01-10 00:36:24 +0100

    add ClientController class

---
+++ controllers/ClientController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * TODO:
+ *    - page homme version connecté
+ *    - fonction de recherche
+*/
+require_once("./conf/Connexion.php");
+Connexion::connect();
+
+class ClientController {
+    public static function home() {
+        return require("./view/home.php");
+    }
+
+    public static function createClient() {
+        return require("./view/register.php");
+    }
+
+    public static function createdClient() {
+        $email = $_GET["email"];
+        $pseudo = $_GET["pseudo"];
+        $pwd = $_GET["pwd"];
+
+        //$client = new Client($email, $pseudo, $pwd);
+
+        $_SESSION["email"] = $email;
+        return header("Location: index.php");
+    }
+
+    public static function connectClient() {
+        return require("./view/connect.php");
+    }
+
+    public static function connectedClient() {
+
+    }
+
+    public static function disconnection() {
+        session_destroy();
+        return header("Location: index.php");
+    }
+
+    public static function update() {
+
+    }
+
+    public static function updated() {
+        
+    }
+}
