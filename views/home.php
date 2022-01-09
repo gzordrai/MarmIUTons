98eaf43de74089b5762543bba7d5125dde057964
gzordrai <40698142+gzordrai@users.noreply.github.com>
2022-01-10 00:44:45 +0100

    update home page

--- views/home.php
+++ views/home.php
@@ -1,7 +1,54 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style/navbar_style.css">
+        <link rel="stylesheet" href="./style/style.css">
+        <link rel="stylesheet" href="./style/title_style.css">
+        <link rel="stylesheet" href="./style/research_style.css">
+        <link rel="stylesheet" href="./style/book_style.css">
+        <link rel="stylesheet" href="./style/add_recipe_style.css">
+        <script type="application/javascript" src="./scripts/home.js"></script>
+        <script type="application/javascript" src="https://kit.fontawesome.com/95f48b9c6d.js" crossorigin="anonymous"></script>
+
+        <title>Recipie</title>
     </head>
     <body>
+        <ul>
+            <li><a class="active" href="#home">Livre de recettes</a></li>
+            <li><a href="#">Recherche</a></li>
+            <?php
+                if(!isset($_SESSION["email"])) {
+            ?>
+                    <li class='connect'><a href='index.php?action=connectClient'>Se connecter</a></li>
+                    <li class='connect'><a href='index.php?action=createClient'>S'inscrire</a></li>
+            <?php
+                } else {
+            ?>
+                    <li class='connect'><a href='index.php?action=profile'>Profil</a></li>
+                    <li class='connect'><a href='index.php?action=disconnection'>Deconnexion</a></li>
+            <?php
+                }
+            ?>
+        </ul>
+        
+        <section id="paralax">
+            <h2 id="text">Recipie</h2>
+            <img src="./images/paralax/background.png" id="background">
+            <img src="./images/paralax/etoile.png" id="etoile">
+            <img src="./images/paralax/lune.png" id="lune">
+            <img src="./images/paralax/nuages.png" id="nuages">
+            <img src="./images/paralax/colline_rose.png" id="colline_rose">
+            <img src="./images/paralax/colline_verte.png" id="colline_verte">
+            <img src="./images/paralax/colline_marron.png" id="colline_marron">
+            <img src="./images/paralax/colline_rouge.png" id="colline_rouge">
+            <img src="./images/paralax/colline_bleue.png" id="colline_bleue">
+            <img src="./images/paralax/sol.png" id="sol">
+        </section>
+
+        <!-- content  -->
+        <div class="main"></div>
     </body>
 </html>
